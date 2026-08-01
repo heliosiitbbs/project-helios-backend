b0123a33ab5104a4881961270b88a5f49701bbcf
praneeth2025 <vamsipraneeth2004@gmail.com>
2026-08-01 14:21:26 +0530

    Solved Timetable Errors

--- routes/timetableRoutes.js
+++ routes/timetableRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getMyTimetable } from "../controllers/timetableController.js";
+import { getMyTimetable, getMyCurriculum, updateMySubjectSelections } from "../controllers/timetableController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/my-timetable", protect, getMyTimetable);
+router.get("/my-curriculum", protect, getMyCurriculum);
+router.patch("/register-subjects", protect, updateMySubjectSelections);
 
 export default router;
