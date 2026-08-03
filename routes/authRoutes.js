6c3109d4bb46dc869f8f8b3563ff8f1c810b1984
praneeth2025 <vamsipraneeth2004@gmail.com>
2026-08-03 19:13:19 +0530

    removed authfailure

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import rateLimit from "express-rate-limit";
 
 import {
     loginUser,
@@ -12,50 +11,33 @@ import {
 
 const router = express.Router();
 
-const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10,
-    standardHeaders: true,
-    legacyHeaders: false,
-    message: {
-        success: false,
-        message: "Too many attempts. Please try again later."
-    }
-});
-
 router.post(
     "/login",
-    authLimiter,
     loginUser
 );
 
 router.post(
     "/update-password",
-    authLimiter,
     updateInitialPassword
 );
 
 router.post(
     "/check-account",
-    authLimiter,
     checkAccountStatus
 );
 
 router.post(
     "/send-verification-code",
-    authLimiter,
     sendAuthVerification
 );
 
 router.post(
     "/verify-code",
-    authLimiter,
     verifyAuthCode
 );
 
 router.post(
     "/setup-pin",
-    authLimiter,
     registerOrUpdateUserPin
 );
 
